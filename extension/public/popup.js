aaf6d779e39f1adf10bb8a5f1045b2e6cc7908b9
Leonard Min <hmin697@aucklanduni.ac.nz>
2022-08-23 21:39:02 +1200

    feat: getters and setters for cookie

--- extension/public/popup.js
+++ extension/public/popup.js
@@ -43,6 +43,34 @@ const blockedWordsData = [
     "pizza"
 ];
 
+//keywords cookie
+const cookieName = "PolityCry";
+var cookieList = getCookie(cookieName);
+
+////set cookie
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+};
+
+////stores cookie in cookielist;
+function getCookie(cname) {
+let name = cname + "=";
+let decodedCookie = decodeURIComponent(document.cookie);
+let ca = decodedCookie.split(';');
+for(let i = 0; i <ca.length; i++) {
+  let c = ca[i];
+  while (c.charAt(0) == ' ') {
+    c = c.substring(1);
+  }
+  if (c.indexOf(name) == 0) {
+    return c.substring(name.length, c.length);
+  }
+}
+};
+
 // UI references
 const allowedBtn = document.getElementById("allowedBtn");
 const blockedBtn = document.getElementById("blockedBtn");
